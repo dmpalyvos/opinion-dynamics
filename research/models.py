a0018b578553f787582cf614837ad98dd069aeda
dmpalyvos <dm.palyvos@gmail.com>
2015-12-05 11:49:18 +0200

    Added kNN_static_nomem function.

--- research/models.py
+++ research/models.py
@@ -458,7 +458,7 @@ def hk(s, op_eps, max_rounds, eps=1e-6, plot=False, conv_stop=True,
 
 def hk_local(A, s, op_eps, max_rounds, eps=1e-6, plot=False, conv_stop=True,
              save=False):
-    '''Simulates the model of Hegselmann-Krause with an Adjacency Matrix
+    '''Simulates the model of Hegselmann-Krause with an Adjacency Matrix.
 
     Contrary to the standard Hegselmann-Krause Model, here we make use of
     an adjacency matrix that represents an underlying social structure
@@ -526,6 +526,66 @@ def hk_local(A, s, op_eps, max_rounds, eps=1e-6, plot=False, conv_stop=True,
                       opinions=opinions[0:t+1, :])
 
     return opinions[0:t+1, :]
+
+
+def hk_local_nomem(A, s, op_eps, max_rounds, eps=1e-6, conv_stop=True):
+    '''Simulates the model of Hegselmann-Krause with an Adjacency Matrix
+
+    Contrary to the standard Hegselmann-Krause Model, here we make use of
+    an adjacency matrix that represents an underlying social structure
+    independent of the opinions held by the members of the society. This
+    variant does not store the intermediate opinions and as a result uses
+    much less memory.
+
+    Args:
+        A (NxN numpy array): Adjacency matrix (its diagonal is the stubborness)
+
+        s (1xN numpy array): Initial opinions (intrinsic beliefs) vector
+
+        op_eps: ε parameter of the model
+
+        max_rounds (int): Maximum number of rounds to simulate
+
+        eps (double): Maximum difference between rounds before we assume that
+        the model has converged (default: 1e-6)
+
+        conv_stop (bool): Stop the simulation if the model has converged
+        (default: True)
+
+    Returns:
+        t, z where t is the convergence time and z the vector of the
+        final opinions.
+
+    '''
+
+    N, z, max_rounds = preprocessArgs(s, max_rounds)
+
+    # All nodes must listen to themselves for the averaging to work
+    A_model = A + np.eye(N)
+
+    z_prev = z.copy()
+    opinions = np.zeros((max_rounds, N))
+    opinions[0, :] = s
+
+    for t in range(1, max_rounds):
+        for i in range(N):
+            # Neighbors in the underlying social network
+            neighbor_i = A_model[i, :] > 0
+            opinion_close = np.abs(z_prev - z_prev[i]) <= op_eps
+            # The node listens to those who share a connection with him
+            # in the underlying network and also have an opinion
+            # which is close to his own
+            friends_i = np.logical_and(neighbor_i, opinion_close)
+            z[i] = np.mean(z_prev[friends_i])
+        opinions[t, :] = z
+        if conv_stop and \
+           norm(z - z_prev, np.inf) < eps:
+            print('Hegselmann-Krause (Local Knowledge) converged after {t} '
+                  'rounds'.format(t=t))
+            break
+        z_prev = z.copy()
+
+    return t, z
 
 
 def kNN_static(A, s, K, max_rounds, eps=1e-6, plot=False, conv_stop=True,
--- research/test.py
+++ research/test.py
@@ -8,11 +8,12 @@ import seaborn
 import models
 from viz import plotNetwork
 from util import gnp, barabasi_albert, from_edgelist
-
+import matplotlib.pyplot as plt
 
 rand.seed(1233)
 A, N = from_edgelist('./networks/facebook_combined.txt')
 s = rand.rand(N)
+max_rounds = 101
 #A = gnp(N, 0.12, rand_weights=True)
 B = diag(rand.rand(N)) * 0.5
 #models.deGroot(A, s, max_rounds, plot=True)
@@ -22,6 +23,8 @@ B = diag(rand.rand(N)) * 0.5
 #models.hk_local(A, s, 0.07, max_rounds, eps=1e-8, plot=True)
 #models.ga(A, B, s, max_rounds, plot=True, save=True)
 #models.kNN_static(A, s, 5, max_rounds, conv_stop=True, plot=True)
-#t, z, Q = models.kNN_dynamic_nomem(A, s, 20, max_rounds)
-#plotNetwork(Q, z)
+#t, z, Q = models.kNN_dynamic_nomem(A, s, 4, max_rounds)
+t, z = models.kNN_static_nomem(A, s, 4, max_rounds)
+#plotNetwork(Q, z, node_size=10, iterations=50)
 #plotNetwork(A, op[-1,:])
+plt.hist(z)
